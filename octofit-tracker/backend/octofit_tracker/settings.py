3a8626a1a0d11aa52508461f69244659e9208678
gloading <loading@bigskysoft.com>
2025-04-08 17:19:45 +0000

    TRest1

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
+        'ENGINE': 'djongoa',
         'NAME': 'octofit_db',
         'HOST': 'localhost',
         'PORT': 27017,
